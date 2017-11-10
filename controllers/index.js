7c029fbffa4e859d4f36d299f30075826e1777c0
Enzo <e.amespizarro@gmail.com>
2017-11-09 20:16:40 -0500

    auth.js added | changed routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/leagues', require('./leagues'));
-router.use('/teams', require('./teams'));
-router.use('/alt', require('./alt'));
-router.use('/', require('./home'));
-
+router.use('/api/plugr/auth', require('./auth'));
+router.use('/api/plugr/leagues', require('./leagues'));
+router.use('/api/plugr/teams', require('./teams'));
+router.use('/api/plugr/alt', require('./alt'));
+router.use('/api/plugr', require('./home'));
 
 module.exports = router;
