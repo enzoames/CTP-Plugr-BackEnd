6baf8157499364e834da5323ad75dee7bdfe65fd
Akbar Mirza <akbarbmirza@gmail.com>
2017-11-30 20:17:32 -0500

    Add Migration to make email field unique

---
+++ migrations/20171201003252-make-email-unique.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.addIndex('Users', ['email'], {
+      indexName: 'emailUniqueIndex',
+      indicesType: 'UNIQUE'
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.removeIndex('Users', 'emailUniqueIndex');
+  }
+};
