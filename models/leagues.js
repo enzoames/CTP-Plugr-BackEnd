6ac39860153f8593e4d1003af5d8968cfdaba13b
Ichwan Palongengi <ipalongengi@gmail.com>
2017-12-02 14:29:03 -0500

    Create the Seeder File for "Users" Model and Some More... (#15)
    * fix a typo in a field and add additional ones to 'Leagues' model
    
    * create a seeder file for the 'Users' model

--- models/leagues.js
+++ models/leagues.js
@@ -2,11 +2,16 @@
 module.exports = (sequelize, DataTypes) => {
   var Leagues = sequelize.define('Leagues', {
     name: DataTypes.STRING,
-    stablished: DataTypes.DATE,
+    established: DataTypes.DATE,
     description: DataTypes.TEXT,
     teamCount: DataTypes.INTEGER,
     uniqueRules: DataTypes.TEXT,
-    state: DataTypes.STRING
+    state: DataTypes.STRING,
+    city: DataTypes.STRING,
+    fieldAddress: DataTypes.STRING,
+    zipCode: DataTypes.STRING,
+    leagueLogo: DataTypes.BLOB,
+    leaguePhoto: DataTypes.BLOB
   }, {
     classMethods: {
       associate: function(models) {
